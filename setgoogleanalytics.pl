fdd0ff68ebedb867fa9657bec2055185d3769daf
mersy <info@mersy.jp>
2011-06-22 16:23:25 +0100

    analytics的なコードに変更。 次はIDを渡す方法。

--- setgoogleanalytics.pl
+++ setgoogleanalytics.pl
@@ -48,7 +48,19 @@ sub set_google_analytics{
 	my ($cb, %args) = @_;
 	my $content_ref = $args{content};
 	my $getanalyticsID = $plugin->option1;
-	$$content_ref =~ s!</body>!<\!-- $getanalyticsID -->\n</body>!;
+	my $analyticsCode =<<'EOT';
+<script type="text/javascript"> 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$getanalyticsID']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script> 
+EOT
+	$$content_ref =~ s!</body>!$analyticsCode</body>!;
 }
 
 
